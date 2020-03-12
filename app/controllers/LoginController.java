f6433b0586afead8efe481bdb83b7e751cbf80a5
Gaon Park <helloproud25@naver.com>
2020-03-12 13:05:29 +0900

    3/12 エラーを表した

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -7,12 +7,14 @@ import play.libs.ws.WSClient;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.Results;
 import repository.UserRepository;
 
 import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
@@ -35,16 +37,14 @@ public class LoginController extends Controller {
         this.context = context;
     }
 
-    public Result index(){
-        return ok("index");
-    }
-
     public Result sendGitOAuth(){
+        //get code from github
         String url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI;
         return redirect(url);
     }
 
     public Result callback(String code, Http.Request request) throws ExecutionException, InterruptedException {
+        // get access_token from github
         CompletionStage<JsonNode> completionStage = ws.url("https://github.com/login/oauth/access_token")
                 .addHeader("Accept", "application/json")
                 .addQueryParameter("code", code)
@@ -57,6 +57,7 @@ public class LoginController extends Controller {
         jsonNode = completionStage.toCompletableFuture().get();
         ACCESS_TOKEN = jsonNode.get("access_token").asText();
 
+        // get user information from github
         completionStage = ws.url("https://api.github.com/user")
                 .addHeader("Authorization", "Token " + ACCESS_TOKEN)
                 .get().thenApply(r -> r.asJson());
@@ -64,6 +65,8 @@ public class LoginController extends Controller {
         jsonNode = completionStage.toCompletableFuture().get();
 
         int id = Integer.parseInt(jsonNode.get("id").toString());
+
+        // Verify that there are users in the database
         CompletionStage<User> userCompletionStage = userRepository.select(id);
         if(userCompletionStage.toCompletableFuture().get() == null) {
             User user = new User();
@@ -82,7 +85,7 @@ public class LoginController extends Controller {
             CompletionStage<User> completionStage = userRepository.select(id);
             return completionStage.thenApplyAsync(user -> ok(toJson(user)), context.current());
         }
-        return null;
+        return CompletableFuture.completedFuture(Results.unauthorized());
     }
 
     public Result logout(Http.Request request){
